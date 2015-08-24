f5a840ef5156d46b20393dbc1fef688079ea9ccb
pulkit21 <pulkit.ag02@gmail.com>
2015-08-24 17:37:15 +0530

    Ripple effect

--- app/src/main/java/com/example/pulkit/materialdesign/NavDrawerAdapter.java
+++ app/src/main/java/com/example/pulkit/materialdesign/NavDrawerAdapter.java
@@ -1,12 +1,14 @@
 package com.example.pulkit.materialdesign;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Collections;
 import java.util.List;
@@ -18,8 +20,16 @@ public class NavDrawerAdapter extends RecyclerView.Adapter<NavDrawerAdapter.View
 
     private LayoutInflater inflater;
     List<DrawerInformation> data = Collections.emptyList();
+    private Context context;
+    private ClickListener clickListener;
+
+
+    public void setClickListener(ClickListener clickListener) {
+        this.clickListener = clickListener;
+    }
 
     public NavDrawerAdapter(Context context, List<DrawerInformation> data){
+        this.context = context;
         inflater = LayoutInflater.from(context);
         this.data = data;
 
@@ -37,7 +47,18 @@ public class NavDrawerAdapter extends RecyclerView.Adapter<NavDrawerAdapter.View
         DrawerInformation current = data.get(i);
         viewHolder.textView.setText(current.title);
         viewHolder.imageView.setImageResource(current.iconId);
+//        viewHolder.imageView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                    Toast.makeText(v.getContext(),"Item clicked" + i, Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
+    }
+
+    public void delete(int position) {
+        data.remove(position);
+        notifyItemRemoved(position);
     }
 
     @Override
@@ -45,7 +66,7 @@ public class NavDrawerAdapter extends RecyclerView.Adapter<NavDrawerAdapter.View
         return data.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView textView;
         ImageView imageView;
 
@@ -53,6 +74,21 @@ public class NavDrawerAdapter extends RecyclerView.Adapter<NavDrawerAdapter.View
             super(itemView);
             textView = (TextView) itemView.findViewById(R.id.nav_list_item);
             imageView = (ImageView) itemView.findViewById(R.id.nav_list_image);
+            itemView.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View v) {
+//            context.startActivity(new Intent(context, TextActivity.class)); //This is not a good way of doing
+                                                                              //stuff it should be in activity or fragment
+//            delete(getAdapterPosition()); //To implement delete operation on list
+            if (clickListener != null){
+                clickListener.itemClicked(v, getAdapterPosition());
+            }
+        }
+    }
+
+    public interface ClickListener {
+        public void itemClicked(View view, int position);
     }
 }
